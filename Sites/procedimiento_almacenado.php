38d88ea537ef2eab050186f0022285b43d8e3598
pablo aldana <pablo.aldana@uc.cl>
2020-05-13 23:22:13 -0400

    pc1

---
+++ Sites/procedimiento_almacenado.php
@@ -0,0 +1,26 @@
+<?php include('../templates/header.html')?>
+
+  <main id="main">
+
+<?php
+
+    require("../config/conexion.php");
+
+    #Se construye la consulta como un string
+    $query = "SELECT DISTINCT nombre FROM artistas;";
+
+    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados 
+    $result = $db_impar -> prepare($query);
+    $result -> execute();
+    $rows = $result -> fetchAll();
+    foreach ($rows as $key => $value){
+        echo '
+        <div class="label-container">
+            <input type="checkbox" name="artista[]" id="'. $value['nombre'] . '" value="Artistas">
+            <label for="Artistas">Artistas</label>
+        </div>
+        ';
+    }
+?>
+
+<?php include("../templates/breadcrumbs.html")?>
