75c1a6a8616ec1a3e2caa3b0d77406b4b18dd87e
Vicente Pumarino <vpumarino@uc.cl>
2020-05-15 01:05:27 -0400

    Avance en crear usuario

--- Sites/index_par.php
+++ Sites/index_par.php
@@ -12,81 +12,25 @@
     <br/><br/>
     <input type="submit" value="Buscar"> 
   </form>
-  <h1 align="center">Consultas </h1>
-
   <br>
-
-  <h3 align="center"> 1. Usuarios junto a su correo</h3>
-
-  <form align="center" action="consultas/consulta_usuarios_correo.php" method="post">
-    <input type="submit" value="Buscar">
-  </form>
-  
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> 2. Ciudades de la agencia pertenecientes a cierto pais</h3>
-
-  <form align="center" action="consultas/consulta_ciudades.php" method="post">
-    Pais:
-    <input type="text" name="pais">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center">3. Paises visitados por el usuario con cierto username </h3>
-
-  <form align="center" action="consultas/consulta_username.php" method="post">
-    Username:
+  <body>
+<h3 align="center">Nuevo Usuario</h3>
+<form align="center" action="usuario/new.php" method="post">
+    Username
     <input type="text" name="username">
     <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center">4. Dinero gastado en tickets por un usuario con cierto id</h3>
-
-  <form align="center" action="consultas/consulta_dinero_tickets.php" method="post">
-    User ID:
-    <input type="text" name="user_id">
+    Email
+    <input type="text" name="email">
     <br/><br/>
-    <input type="submit" value="Buscar">
+    Nombre Completo
+    <input type="text" name="nombre">
+    <br/><br/>
+    Dirección
+    <input type="text" name="direccion">
+    <br/><br/>
+
+    <input type="submit" value="Crear"> 
   </form>
+  <h1 align="center">Consultas </h1>
   <br>
-  <br>
-
-  <h3 align="center">5. Reservas hechas entre el 1 de Enero de 2020 y 31 de Marzo de 2020</h3>
-
-  <form align="center" action="consultas/consulta_enero_marzo.php" method="post">
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-
-  <h3 align="center">6. Dinero gastado en reservas de hoteles por cada usuario entre 2 fechas</h3>
-
-<form align="center" action="consultas/consulta_dinero_hoteles.php" method="post">
-  Fecha Inicio
-  <input type="text" name="F1">
-  <br/><br/>
-  Fecha Fin
-  <input type="text" name="F2">
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
-<br>
-<br>
-
-  <br>
-  <br>
-  <br>
-  <br>
-</body>
-</html>
+  <h2><a href = "consultas/consultas_impar/consultas.php">Ver consultas</a></h2>
---
+++ Sites/usuario/new.php
@@ -0,0 +1,20 @@
+<?php require("../config/conexion.php");
+  session_start();
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $nombreusuario = $_POST["nombre"];
+    $direccion = $_POST["direccion"];
+    $query = "SELECT usuarios.uid FROM usuarios ORDER BY usuarios.uid DESC;";
+    $result = $db_impar -> prepare($query);
+    $result -> execute();
+    $uid = $result -> fetchAll();
+    $rows = 0;
+    $uid = $uid[0][0];
+    $uid++;
+    $query = "INSERT INTO usuarios(uid, nombreusuario, username, correo, direccion) VALUES ($uid, $nombreusuario, $username, $email, $direccion);";
+    $result = $db_impar -> prepare($query);
+    $result -> execute();
+    echo $uid;
+  }
+?>
