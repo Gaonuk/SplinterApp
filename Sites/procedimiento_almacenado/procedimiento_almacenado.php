3df4fa89d100c0fa1522adbb6d979156cc1a388c
pablo aldana <pablo.aldana@uc.cl>
2020-05-16 17:47:06 -0400

    p_a

--- Sites/procedimiento_almacenado/procedimiento_almacenado.php
+++ Sites/procedimiento_almacenado/procedimiento_almacenado.php
@@ -1,26 +1,26 @@
-<?php include('templates/header.html')?>
+<?php include('../templates/header.html')?>
 
   <main id="main">
 
 <?php
 
-    require("config/conexion.php");
+    require("../config/conexion.php");
 
     #Se construye la consulta como un string
     $query = "SELECT DISTINCT nombre FROM artistas;";
 
-    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados 
+    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
     $result = $db_par -> prepare($query);
     $result -> execute();
     $rows = $result -> fetchAll();
     foreach ($rows as $key => $value){
         echo '
         <div class="label-container">
-            <input type="checkbox" name="artista[]" id="'. $value['nombre'] . '" value="Artistas">
+            <input type="checkbox" name="artista[]" id="'. $value['nombre'] . '" value="'. $value['nombre'] .'">
             <label for="Artistas">Artistas</label>
         </div>
         ';
     }
 ?>
 
-<?php include("templates/breadcrumbs.html")?>
+<?php include("../templates/breadcrumbs.html")?>
