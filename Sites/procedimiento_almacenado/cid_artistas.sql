a9c5e1aeac6a723827f88ff73306ca3b4ccc8cd9
pablo aldana <pablo.aldana@uc.cl>
2020-05-16 19:02:17 -0400

    ciudad segun artista

---
+++ Sites/procedimiento_almacenado/cid_artistas.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION cid_artistas(lista_artistas)
+RETURNS TABLE (cid INT, artista VARCHAR) AS
+$$
+BEGIN
+RETURN QUERY EXECUTE 'SELECT DISTINCT ubica_en.cid, artistas.nombre
+    FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
+    WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid
+    AND artistas.nombre IN $1'
+        USING lista_artistas;
+    RETURNS;
+END;
+$$ language plpgsql;
