c4c4e7a1a7db3d06cb7337d077b5ee1b46d65cd1
pablo aldana <pablo.aldana@uc.cl>
2020-05-17 16:21:13 -0400

    PA Sumbit

---
+++ Sites/procedimiento_almacenado/form_procedimiento_almacenado.php
@@ -0,0 +1,63 @@
+<?php include('../templates/header.html')?>
+
+  <main id="main">
+
+<form action="procedimiento_almacenado.php" method="POST">
+
+<!--CheckBox para nombre de artistas-->
+    <label for="nombre_artistas">Artistas:</label><br>
+    <?php
+
+        require("../config/conexion.php");
+
+        #Se construye la consulta como un string
+        $query = "SELECT DISTINCT nombre FROM artistas;";
+
+        #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+        $result = $db_par -> prepare($query);
+        $result -> execute();
+        $rows = $result -> fetchAll();
+        foreach ($rows as $key => $value){
+            echo '
+            <div class="label-container">
+                <input type="checkbox" id="'. $value['nombre'] .'" value="'. $value['nombre'] .'" name="'. $value['nombre'] .'">
+                <label for="'. $value['nombre'] .'">  '.$value['nombre'].'
+                </label><br>
+            </div>
+            ';
+        }
+
+    ?>
+
+<!--Datalist para la ciudad de origen-->
+    <label for="ciudad_origen">Ciudad de Origen:</label><br>
+    <input list="ciudades" name="ciudad_origen">
+    <datalist id="ciudades">
+    <?php
+
+        require("../config/conexion.php");
+        $query = "SELECT DISTINCT nombre FROM ciudades;";
+
+        #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+        $result = $db_par -> prepare($query);
+        $result -> execute();
+        $rows = $result -> fetchAll();
+        foreach ($rows as $key => $value){
+            echo'
+            <option value=" '. $value['nombre'] .' ">
+            ';
+        }
+    ?>
+    </datalist><br>
+
+<!--Date para la fecha de inicio-->
+    <label for="fecha">Fecha:</fecha><br>
+    <input type="date" id="fecha" name="fecha">
+
+<!--Sumbit boton envia a procedimiento_almacenado.php -->
+    <input type="sumbit" value="Buscar" class="boton-busqueda">
+
+
+</form>
+
+<?php include("../templates/breadcrumbs.html")?>
--- Sites/procedimiento_almacenado/procedimiento_almacenado.php
+++ Sites/procedimiento_almacenado/procedimiento_almacenado.php
@@ -2,58 +2,13 @@
 
   <main id="main">
 
-<form>
+<?php
+    $ciudad_origen = $_POST["ciudad_origen"]
+    $fecha = $_POST["fecha"]
 
-<!--CheckBox para nombre de artistas-->
-    <label for="nombre_artistas">Artistas:</label><br>
-    <?php
+?>
 
-        require("../config/conexion.php");
+<?php
+    <p>La fecha es '$fecha' y la ciudad '$ciudad_origen'</p>
 
-        #Se construye la consulta como un string
-        $query = "SELECT DISTINCT nombre FROM artistas;";
-
-        #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-        $result = $db_par -> prepare($query);
-        $result -> execute();
-        $rows = $result -> fetchAll();
-        foreach ($rows as $key => $value){
-            echo '
-            <div class="label-container">
-                <input type="checkbox" id="nombre_artista" value="'. $value['nombre'] .'" name="'. $value['nombre'] .'">
-                <label for="'. $value['nombre'] .'">  '.$value['nombre'].'
-                </label><br>
-            </div>
-            ';
-        }
-
-    ?>
-
-<!--Datalist para la ciudad de origen-->
-    <label for="ciudad_origen">Ciudad de Origen:</label><br>
-    <input list="ciudades">
-    <datalist id="ciudades">
-    <?php
-
-        require("../config/conexion.php");
-        $query = "SELECT DISTINCT nombre FROM ciudades;";
-
-        #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-        $result = $db_par -> prepare($query);
-        $result -> execute();
-        $rows = $result -> fetchAll();
-        foreach ($rows as $key => $value){
-            echo'
-            <option value=" '. $value['nombre'] .' ">
-            ';
-        }
-    ?>
-    </datalist><br>
-
-<!--Date para la fecha de inicio-->
-    <label for="fecha">Fecha:</fecha><br>
-    <input type="date" id="fecha" name="fecha">
-
-</form>
-
-<?php include("../templates/breadcrumbs.html")?>
+?>
