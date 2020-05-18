5e58e195dfab4f1de138073f3e51d6bb79a5c760
pablo aldana <pablo.aldana@uc.cl>
2020-05-18 18:45:41 -0400

    PA consulta cid nombre fix3

--- Sites/procedimiento_almacenado/procedimiento_almacenado.php
+++ Sites/procedimiento_almacenado/procedimiento_almacenado.php
@@ -15,24 +15,21 @@
         array_push($nombre_artistas, $value );
       }
     }
-?>
 
-<?php
+    require("../config/conexion.php");
 
-require("../config/conexion.php");
+    #Se construye la consulta como un string
+    $query = 'SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
+    FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
+    WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid
+    AND artistas.nombre IN (\''.implode("','", $nombre_artista).'\')';
 
-#Se construye la consulta como un string
-$query = 'SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
-FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
-WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid
-AND artistas.nombre IN (\''.implode("','", $nombre_artista).'\')';
-
-#Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-$result = $db_par -> prepare($query);
-$result -> execute();
-$rows = $result -> fetchAll();
-foreach ($rows as $key => $value){
-    echo ' ' .$value['cid'] . ' de '.$value['nombre'] .' ';
-}
+    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+    $result = $db_par -> prepare($query);
+    $result -> execute();
+    $rows = $result -> fetchAll();
+    foreach ($rows as $key => $value){
+        echo ' ' .$value['cid'] . ' de '.$value['nombre'] .' ';
+    }
 
 ?>
