cfe4049d40f7a72616dbf0d22c2d93cb58c6ae9a
pablo aldana <pablo.aldana@uc.cl>
2020-05-26 00:53:26 -0400

    itinerario2 array

--- Sites/procedimiento_almacenado/itinerario2.sql
+++ Sites/procedimiento_almacenado/itinerario2.sql
@@ -6,7 +6,9 @@ RETURN QUERY SELECT *
     SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
     FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
     WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid'
-    ) AS artistas_cid(cid int, nombre varchar(50)) WHERE artistas_cid.nombre IN string_to_array(nombre_artistas, ',');
+    ) AS artistas_cid(cid int, nombre varchar(50)),
+    string_to_array(nombre_artistas, ',') AS nombre_array(nombre)
+    WHERE artistas_cid.nombre =ANY(nombre_array.nombre);
 RETURN;
 END;
 $$ language plpgsql;
