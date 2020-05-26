22fe4b33bc1daa751f206857b49c9aa7b9ea3e1f
pablo aldana <pablo.aldana@uc.cl>
2020-05-26 00:36:28 -0400

    itinerario2 string to array

--- Sites/procedimiento_almacenado/itinerario2.sql
+++ Sites/procedimiento_almacenado/itinerario2.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE FUNCTION itinerario(fecha date, origen varchar, nombre_artistas varchar)
+DECLARE array_nombres 
 RETURNS Table (cid int, nombre varchar(50)) as $$
 BEGIN
 RETURN QUERY SELECT *
@@ -6,7 +7,7 @@ RETURN QUERY SELECT *
     SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
     FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
     WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid'
-    ) AS artistas_cid(cid int, nombre varchar(50)) WHERE artistas_cid.nombre IN (nombre_artistas);
+    ) AS artistas_cid(cid int, nombre varchar(50)) WHERE artistas_cid.nombre IN string_to_array(nombre_artistas, ',');
 RETURN;
 END;
 $$ language plpgsql;
