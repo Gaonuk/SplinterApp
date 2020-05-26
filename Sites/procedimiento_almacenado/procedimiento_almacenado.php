1ec0dde11e2f688de2eca53d4318703e1f652902
pablo aldana <pablo.aldana@uc.cl>
2020-05-26 17:57:27 -0400

    PA fix

--- Sites/procedimiento_almacenado/procedimiento_almacenado.php
+++ Sites/procedimiento_almacenado/procedimiento_almacenado.php
@@ -40,7 +40,7 @@
 
     */
     echo ' '.$ciudad_origen.'  '.$fecha.'  '.$nombre_artistas.' ';
-    $query = 'SELECT * FROM itinerario('$fecha', '$ciudad_origen', 'implode(",", $nombre_artistas)')';
+    $query = 'SELECT * FROM itinerario('.$fecha.', '.$ciudad_origen.', '.implode(",", $nombre_artistas).')';
 
     #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
     $result = $db_impar -> prepare($query);
