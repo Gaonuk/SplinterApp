379d9486ea52ce326209d27b93e3bd7171c62d16
pablo aldana <pablo.aldana@uc.cl>
2020-05-26 17:29:37 -0400

    itinerario dbname

--- Sites/procedimiento_almacenado/itinerario.sql
+++ Sites/procedimiento_almacenado/itinerario.sql
@@ -8,7 +8,7 @@ BEGIN
 	CREATE TEMP TABLE IF NOT EXISTS ciudades_id AS
         (SELECT DISTINCT cid_artistas.cid FROM 
 		(SELECT artistas_cid.cid, artistas_cid.nombre
-		FROM dblink('dbname=grupo84 user=grupo84 password=grupo84', '
+		FROM dblink('dbname=grupo84e3 user=grupo84 password=grupo84', '
 		SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
 		FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
 		WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid'
