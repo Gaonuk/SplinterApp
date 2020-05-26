93cf374bf0eba6a50779b3ad517440af6c573eff
pablo aldana <pablo.aldana@uc.cl>
2020-05-25 22:04:08 -0400

    itinerario2 using

--- Sites/procedimiento_almacenado/itinerario2.sql
+++ Sites/procedimiento_almacenado/itinerario2.sql
@@ -1,14 +1,14 @@
 CREATE OR REPLACE FUNCTION itinerario(fecha date, origen varchar, nombre_artistas varchar)
 RETURNS Table (cid int, nombre varchar(50)) as $$
 BEGIN
-RETURN QUERY SELECT *
+RETURN QUERY EXECUTE 'SELECT *
     FROM dblink('dbname=grupo84e3 user=grupo84 password=grupo84', '
     SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
     FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
     WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid
     AND artistas.nombre IN ($1)'
+    ) AS artistas(cid int, nombre varchar(50))'
     USING nombre_artistas;
-    ) AS artistas(cid int, nombre varchar(50));
 RETURN;
 END;
 $$ language plpgsql;
