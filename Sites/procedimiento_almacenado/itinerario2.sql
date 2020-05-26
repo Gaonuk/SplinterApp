dbff49152e9e3cd8cd1452dd0e48bb8b31d95ee0
pablo aldana <pablo.aldana@uc.cl>
2020-05-25 21:52:48 -0400

    itinerario2 fix

--- Sites/procedimiento_almacenado/itinerario2.sql
+++ Sites/procedimiento_almacenado/itinerario2.sql
@@ -5,7 +5,7 @@ BEGIN
     SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
     FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
     WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid
-    AND artistas.nombre IN  (nombre_artistas)
-    ) AS artistas(cid int, nombre varchar(50))');
+    AND artistas.nombre IN (nombre_artistas)'
+    ) AS artistas(cid int, nombre varchar(50));
 
 $$ language plpgsql;
