cf12c702cb8bf43537e2b362b1f6dad4be1fb1c9
pablo aldana <pablo.aldana@uc.cl>
2020-05-25 23:52:38 -0400

    itinerario2 nombre_artista

--- Sites/procedimiento_almacenado/itinerario2.sql
+++ Sites/procedimiento_almacenado/itinerario2.sql
@@ -5,10 +5,8 @@ RETURN QUERY SELECT *
     FROM dblink('dbname=grupo84e3 user=grupo84 password=grupo84', '
     SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
     FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
-    WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid
-    AND artistas.nombre IN ($1)'
-    ) AS artistas(cid int, nombre varchar(50))
-    USING nombre_artistas;
+    WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid'
+    ) AS artistas(cid int, nombre varchar(50)) WHERE nombre IN nombre_artistas;
 RETURN;
 END;
 $$ language plpgsql;
