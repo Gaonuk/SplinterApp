74ff44da39afc8e59917401085e12d436dd796c8
Vicente Pumarino <vpumarino@uc.cl>
2020-05-26 02:50:29 -0400

    agregando dropdown

--- Sites/usuario/nuevo_ticket.php
+++ Sites/usuario/nuevo_ticket.php
@@ -11,17 +11,23 @@ Comprar Tickets
   Ciudad origen: 
      <select Nombre Ciudad='NEW'> 
      <option value="">--- Select ---</option> 
-    <?php include("../config/conexion.php");
-	       $result = pg_query($db_impar, "SELECT cid, nombreciudad from ciudades ");
-	       while($row_list=pg_fetch_assoc($result)){
-	         ?>
-         <option value="<?php echo $row_list["cid"]; ?>">
-         <?php echo $row_list["nombreciudad"]; ?>
-         </option>
+    <?php 
+      include("../config/conexion.php");
+      $result = pg_query($db_impar, "SELECT cid, nombreciudad from ciudades ");
+      if (pg_num_rows($result) == 0)
+    {
+     echo "<option>No rows exist</option>";
+    } else {
+      while($row_list=pg_fetch_assoc($result)){
+        ?>
+      <option value="<?php echo $row_list["cid"]; ?>">
+      <?php echo $row_list["nombreciudad"]; ?>
+      </option>
 <?php
-	         }
-	         ?>
-         </select>
-     <input type="submit" value="Buscar">
+      }
+    }
+      ?>
+    </select>
+  <input type="submit" value="Buscar">
  </form>
  <br>
