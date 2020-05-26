f36f4d5b4ba2aca71296f719c295ab76d628a481
pablo aldana <pablo.aldana@uc.cl>
2020-05-25 23:56:23 -0400

    itinerario2 parentesis

--- Sites/procedimiento_almacenado/itinerario2.sql
+++ Sites/procedimiento_almacenado/itinerario2.sql
@@ -6,7 +6,7 @@ RETURN QUERY SELECT *
     SELECT DISTINCT ubica_en.cid AS cid, artistas.nombre AS nombre
     FROM artistas, ciudades, obras, obras_en, realizo, ubica_en
     WHERE artistas.aid = realizo.aid AND obras.oid = realizo.oid AND obras.oid = obras_en.oid AND ubica_en.lid = obras_en.lid'
-    ) AS artistas(cid int, nombre varchar(50)) WHERE nombre IN nombre_artistas;
+    ) AS artistas(cid int, nombre varchar(50)) WHERE nombre IN (nombre_artistas);
 RETURN;
 END;
 $$ language plpgsql;
