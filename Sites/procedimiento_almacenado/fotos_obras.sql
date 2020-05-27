29b81e453bac179bffeb4d78339e74dd2e263753
pablo aldana <pablo.aldana@uc.cl>
2020-05-27 17:06:29 -0400

    fotos_obras

--- Sites/procedimiento_almacenado/fotos_obras.sql
+++ Sites/procedimiento_almacenado/fotos_obras.sql
@@ -13,23 +13,23 @@ BEGIN
     UPDATE obras SET foto_url= 'https://en.wikipedia.org/wiki/Penitent_Magdalene_(Donatello)#/media/File:Donatello,_maria_maddalena_02.JPG' WHERE oid=10;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/La_Libertad_guiando_al_pueblo#/media/Archivo:Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg' WHERE oid=11;
     UPDATE obras SET foto_url= 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Giorgio_Vasari_-_The_Last_Judgment_-_WGA24313.jpg' WHERE oid=12;
-    UPDATE obras SET foto_url= '' WHERE oid=13;
-    UPDATE obras SET foto_url= '' WHERE oid=14;
+    UPDATE obras SET foto_url= ' ' WHERE oid=13;
+    UPDATE obras SET foto_url= ' ' WHERE oid=14;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Fuente_de_Neptuno_(Plaza_Navona,_Roma)#/media/Archivo:Fountain_of_Neptune,_Rome.jpg' WHERE oid=15;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Fuente_de_los_Cuatro_R%C3%ADos#/media/Archivo:Lazio_Roma_Navona2_tango7174.jpg' WHERE oid=16;
-    UPDATE obras SET foto_url= '' WHERE oid=17;
+    UPDATE obras SET foto_url= ' ' WHERE oid=17;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/%C3%89xtasis_de_Santa_Teresa#/media/Archivo:Ecstasy_St_Theresa_SM_della_Vittoria.jpg' WHERE oid=18;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Obelisco_de_la_Piazza_della_Minerva#/media/Archivo:Minerveo_obelisk_in_front_of_Santa_Maria_sopra_Minerva.jpg' WHERE oid=19;
     UPDATE obras SET foto_url= 'https://c8.alamy.com/compes/mf5rmy/la-columnata-de-gian-lorenzo-bernini-en-la-plaza-de-san-pedro-y-carlo-maderno-fachada-del-renacimiento-italiano-papale-basilica-maggiore-di-san-pietro-en-vatic-mf5rmy.jpg' WHERE oid=20;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Apolo_y_Dafne_(Bernini)#/media/Archivo:Apollo_and_Daphne_(Bernini).jpg' WHERE oid=21;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Maest%C3%A0_di_Ognissanti#/media/Archivo:Giotto_di_Bondone_090.jpg' WHERE oid=22;
-    UPDATE obras SET foto_url= '' WHERE oid=23;
+    UPDATE obras SET foto_url= ' ' WHERE oid=23;
     UPDATE obras SET foto_url= 'https://it.wikipedia.org/wiki/Polittico_di_Badia#/media/File:Giotto._The_Badia_Polyptych._c._1300._91x340cm._Uffizi,_Florence..jpg' WHERE oid=24;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/La_consagraci%C3%B3n_de_Napole%C3%B3n#/media/Archivo:Jacques-Louis_David_-_The_Coronation_of_Napoleon_(1805-1807).jpg' WHERE oid=25;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Manneken_Pis#/media/Archivo:Bruxelles_Manneken_Pis.jpg' WHERE oid=26;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/La_%C3%BAltima_cena_(Leonardo_da_Vinci)#/media/Archivo:The_Last_Supper_-_Leonardo_Da_Vinci_-_High_Resolution_32x16.jpg' WHERE oid=27;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/La_Gioconda#/media/Archivo:Leonardo_da_Vinci_-_Mona_Lisa_(Louvre,_Paris).jpg' WHERE oid=28;
-    UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Piedad_del_Vaticano#/media/Archivo:Michelangelo\'s_Pieta_5450_cropncleaned.jpg' WHERE oid=29;
+    UPDATE obras SET foto_url= 'https://cdn.culturagenial.com/es/imagenes/michelangelo-s-pieta-5450-cut-out-black-cke.jpg' WHERE oid=29;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Archivo:Miguel_%C3%81ngel,_por_Daniele_da_Volterra_(detalle).jpg' WHERE oid=31;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/La_creaci%C3%B3n_de_Ad%C3%A1n#/media/Archivo:Michelangelo_-_Creation_of_Adam_(cropped).jpg' WHERE oid=32;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/El_Juicio_Final_(Capilla_Sixtina)#/media/Archivo:Last_Judgement_(Michelangelo).jpg' WHERE oid=33;
@@ -38,6 +38,6 @@ BEGIN
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Madonna_Sixtina_(Rafael)#/media/Archivo:RAFAEL_-_Madonna_Sixtina_(Gem%C3%A4ldegalerie_Alter_Meister,_Dresden,_1513-14._%C3%93leo_sobre_lienzo,_265_x_196_cm).jpg' WHERE oid=36;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/Las_Gracias_(Rafael)#/media/Archivo:Rapha%C3%ABl_-_Les_Trois_Gr%C3%A2ces_-_Google_Art_Project_2.jpg' WHERE oid=37;
     UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/La_primavera_(Botticelli)#/media/Archivo:Sandro_Botticelli_-_La_Primavera_-_Google_Art_Project.jpg' WHERE oid=38;
-    UPDATE obras SET foto_url= "https://es.wikipedia.org/wiki/El_nacimiento_de_Venus_(Botticelli)#/media/Archivo:Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg" WHERE oid=39;
+    UPDATE obras SET foto_url= 'https://es.wikipedia.org/wiki/El_nacimiento_de_Venus_(Botticelli)#/media/Archivo:Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg' WHERE oid=39;
 END
 $$ language plpgsql
