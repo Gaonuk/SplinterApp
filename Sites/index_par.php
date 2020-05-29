018a4d9993e57741692f95083341395da9dc5142
Rodrigo Gaona Gonzalez <rodrigogaonagonzalez@gmail.com>
2020-05-29 14:08:41 -0400

    hotfix

--- Sites/index_par.php
+++ Sites/index_par.php
@@ -28,7 +28,7 @@
 <nav class="navbar has-shadow">
 	<div class="navbar-brand">
 		<a class="navbar-item" href="#">
-			<img src="../assets/img/logo_real.png" alt="Logo DB">
+			<img src="assets/img/logo_real.png" alt="Logo DB">
 			<!--        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">-->
 		</a>
 		<!--      <div class="navbar-item">-->
