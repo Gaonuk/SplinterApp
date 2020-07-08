7b22722ccbc9cbc29ab5d3123e7e5dc126d1044b
pablo aldana <pablo.aldana@uc.cl>
2020-07-07 23:11:12 -0400

    fix ;

--- Sites/usuario/text_search.php
+++ Sites/usuario/text_search.php
@@ -19,7 +19,7 @@
           'content' => json_encode( $data ),
           'header'=>  "Content-Type: application/json\r\n" .
                       "Accept: application/json\r\n"
-        );
+        )
       );
     $context  = stream_context_create( $options );
     $result = file_get_contents( $url, false, $context );
