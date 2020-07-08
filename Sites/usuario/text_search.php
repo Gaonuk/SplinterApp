74dc211287f9720aef3a64efe4d0690da11f648b
pablo aldana <pablo.aldana@uc.cl>
2020-07-07 23:25:00 -0400

    tabla de text_search

--- Sites/usuario/text_search.php
+++ Sites/usuario/text_search.php
@@ -11,7 +11,7 @@
         'desired' => explode(';', $desired),
         'required' => explode(';', $required),
         'forbidden' => explode(';', $forbidden),
-        'uid' => $uid
+        'userId' => $uid
     );
     $options = array(
         'http' => array(
@@ -22,8 +22,186 @@
         )
       );
     $context  = stream_context_create( $options );
-    $result = file_get_contents( $url, false, $context );
-    $response = json_decode( $result );
-    print_r($response);
+    $body_r = file_get_contents( $url, false, $context );
+    $body = json_decode( $result );
 ?>
 
+
+<div class="navbar-menu">
+		<div class="navbar-end">
+			<div class="navbar-item has-dropdown is-hoverable">
+				<div class="navbar-link">
+					<figure class="image is-24x24 is-rounded">
+						<img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" alt="">
+					</figure>
+					<?php echo $_SESSION["user"] ?>
+				</div>
+				<div class="navbar-dropdown">
+					<a class="navbar-item" href="logout.php">
+						<div>
+                                <span class="icon is-small">
+                                  <i class="fa fa-sign-out"></i>
+                                </span>
+							Sign Out
+						</div>
+					</a>
+					<a class="navbar-item" href="delete.php">
+						<div>
+                                <span class="icon is-small">
+                                  <i class="fa fa-ban"></i>
+                                </span>
+							Delete Account
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	</nav>
+
+	<section class="">
+		<div class="columns is-gapless">
+			<div class="column is-4-tablet is-3-desktop is-2-widescreen">
+				<div style="background-color: #f5f5f5; padding: 1.5rem; min-height: calc(100vh - 3.25rem); overflow: auto;">
+					<aside class="menu ">
+						<p class="menu-label">
+							Buscar en la aplicacion
+						</p>
+						<form method="post" name="search" action="search.php">
+							<div class="field has-addons">
+								<div class="control has-icons-left">
+									<input class="input is-rounded" type="text" placeholder="Buscar artista, lugar..." name="nombre">
+									<span class="icon is-small is-left">
+										<i class="fas fa-search"></i>
+									</span>
+								</div>
+								<div class="control">
+									<input type="submit" class="button is-info" value="Buscar">
+								</div>
+							</div>
+						</form>
+						<p class="menu-label">
+							General
+						</p>
+						<ul class="menu-list">
+							<li>
+								<a class="is-active">
+													<span class="icon">
+															<i class="fa fa-user"></i>
+													</span>
+									Mi Perfil
+								</a>
+							</li>
+							<li>
+								<a href="artistas.php">
+													<span class="icon">
+															<i class="fa fa-paint-brush"></i>
+													</span>
+									Artistas
+								</a>
+							</li>
+							<li>
+								<a href="obras.php">
+													<span class="icon">
+															<i class="fa fa-image"></i>
+													</span>
+									Obras
+								</a>
+							</li>
+							<li>
+								<a href="lugares.php">
+													<span class="icon">
+															<i class="fas fa-map-marker-alt"></i>
+													</span>
+									Lugares
+								</a>
+							</li>
+						</ul>
+						<p class="menu-label">
+							Itinerario
+						</p>
+						<ul class="menu-list">
+							<li>
+								<a href="../procedimiento_almacenado/form_procedimiento_almacenado.php">
+								<span class="icon">
+									<i class="fas fa-clipboard-list"></i>
+								</span>
+									Crear Itinerario
+								</a>
+							</li>
+						</ul>
+						<p class="menu-label">
+							Compras
+						</p>
+						<ul class="menu-list">
+							<li>
+								<a href="nuevo_ticket.php">
+								<span class="icon">
+									<i class="fas fa-ticket-alt"></i>
+								</span>
+									Comprar Tickets
+								</a>
+							</li>
+							<li>
+								<a href="nuevo_hotel.php">
+								<span class="icon">
+									<i class="fas fa-hotel"></i>
+								</span>
+									Hacer Reserva de Hotel
+								</a>
+							</li>
+						</ul>
+					</aside>
+				</div>
+			</div>
+			<main class="column">
+				<div class="hero is-primary">
+					<div class="hero-body">
+						<h1 class="title">
+							Hola, <?php echo $_SESSION["user"]; ?>
+						</h1>
+						<h2 class="subtitle">
+							Aquí puedes ver los resultados de la busqueda
+						</h2>
+					</div>
+				</div>
+				<div class="section">
+
+					<div class="columns is-centered">
+						<table class="table">
+							<thead>
+							<tr>
+								<!-- Se crea la tabla -->
+                                <th>Fecha</th>
+                                <th>Remitente</th>
+                                <th>Destinatario</th>
+								<th>Mensaje</th>
+							</tr>
+							</thead>
+							<tbody>
+							<?php
+							if ($body_r == 'Invalid ID, no user with Id = ' . $uid) {
+								# Caso donde no hay msjes con ese id
+								echo 'Id de usuario invalido';
+							} else { ?>
+								<?php foreach ($body as $mensaje) { ?>
+									<tr>
+                                        <td><?php echo $mensaje -> date ?></td>
+                                        <tf><?php echo $mensaje -> sender ?></td>
+										<td><?php echo $mensaje -> receptant ?></td>
+										<td><?php echo $mensaje -> message ?></td>
+									</tr>
+									<?php }
+							}?>
+
+							</tbody>
+						</table>
+					</div>
+					<div class="content has-text-centered">
+						<a href="main.php" class="button is-link">Volver</a>
+					</div>
+				</div>
+			</main>
+		</div>
+	</section>
+<?php include "../templates/main_footer.html"; ?>
